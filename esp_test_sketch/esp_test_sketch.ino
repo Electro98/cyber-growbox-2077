fdebb5adc9bd8ff20e1be4d663c068fcb242dc3e
Electro98-main-frame <geny_turov@mail.ru>
2021-05-18 17:07:07 +0700

    Подготовка к отправке данных на сервер

--- esp_test_sketch/esp_test_sketch.ino
+++ esp_test_sketch/esp_test_sketch.ino
@@ -14,9 +14,12 @@ const char* password = STAPSK;
 const int led = 13;
 
 const uint32_t capacity = JSON_OBJECT_SIZE(5) + JSON_ARRAY_SIZE(2)+20;
-StaticJsonDocument<capacity> comDoc;
+StaticJsonDocument<capacity> jsonBuffer;
 
 uint32_t commands_timer = 0;
+
+#define NUM_SENSORS 1
+String sensorsNames[] = {"", "", "", "tds"};
 
 void sendCommandA(byte command, uint32_t comArg){
   Serial.write(command);
@@ -36,21 +39,22 @@ void getServersCommand() {
       int httpCode = http.GET();
       if (httpCode > 0) {
         if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          deserializeJson(comDoc, http.getString());
-          int light0 = comDoc["light"][0];
-          int light1 = comDoc["light"][1];
+          deserializeJson(jsonBuffer, http.getString());
+          int light0 = jsonBuffer["light"][0];
+          int light1 = jsonBuffer["light"][1];
           Serial.println(light0);
           Serial.println(light1);
           sendCommandA(light0 > 0? 3: 4, 1);
           sendCommandA(light1 > 0? 3: 4, 2);
-          if (comDoc.containsKey("motors")) {
-            int32_t motorsSteps = comDoc["motors"];
+          if (jsonBuffer.containsKey("motors")) {
+            int32_t motorsSteps = jsonBuffer["motors"];
             sendCommandA(motorsSteps > 0? 1: 2, abs(motorsSteps));
           }
-          if (comDoc.containsKey("control")) {
-            uint8_t control = comDoc["control"];
+          if (jsonBuffer.containsKey("control")) {
+            uint8_t control = jsonBuffer["control"];
             sendCommandA(5, control);
           }
+          jsonBuffer.clear();
         }
       }
       http.end();
@@ -60,14 +64,22 @@ void getServersCommand() {
 }
 
 void sendSensors() {
-  if (WiFi.status() == WL_CONNECTED){
+  if (WiFi.status() == WL_CONNECTED && Serial.available() > 1){
     digitalWrite(led, 1);
     WiFiClient client;
     HTTPClient http;
     if (http.begin(client, "http://192.168.43.49:5000/data")) {
       http.addHeader("Content-Type", "application/json");
-      // TODO: Тут добавить получение данных с Arduino
-      int httpCode = http.POST("{\"date\":\"2021-04-07\",\"time\":\"16:54\"}");
+      for (int i = 0; i < NUM_SENSORS; i++) {
+        float data = 0;
+        while (Serial.available() <= 1);
+        uint8_t sensorNum = Serial.read();
+        uint8_t dataLen = Serial.read();
+        Serial.readBytes((byte*) &data, dataLen);
+        jsonBuffer[sensorsNames[sensorNum-1]] = data;
+      }
+      
+      int httpCode = http.POST(jsonBuffer.as<String>());
 
       if (httpCode > 0) {
         if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
@@ -76,6 +88,7 @@ void sendSensors() {
         }
       }
       http.end();
+      jsonBuffer.clear();
     }
     digitalWrite(led, 0);
   }
